f019ce6c66977308675d53f140ce1b9f260d598b
JorgeLuis <estebanracing21@gmail.com>
2022-01-08 14:30:54 -0600

    primer archivo de operaciones

---
+++ js/operaciones.js
@@ -0,0 +1,35 @@
+function HacerOperacion() {
+  let primerNumero = Number(document.getElementById("numero1").value) 
+  let segundoNumero =Number(document.getElementById ("numero2").value)
+  let listaOperaciones =document.getElementById ("lista")
+  let tipoOperacion = listaOperaciones.options[listaOperaciones.selectedIndex].value
+  let resultado= 0
+  ////////////////////////////////////
+  let calificacion=-14
+  if (calificacion ===10 || calificacion === 9) {
+    console.log("Excelente")
+  } 
+  else  if (calificacion >=6 && calificacion <=8) {
+   console.log("aprobado")
+  } 
+  else  if (calificacion >10 || calificacion <0) {
+     console.log("Calificacion Invalida")
+  }
+  else {
+    console.log ("reprobado")
+  } 
+} 
+function calculaCalificacion (){
+  ///////////////////////////////
+    let calificacion = -14;
+    if (calificacion === 10 || calificacion === 9) {
+      console.log("Excelente");
+    } else if (calificacion >= 6 && calificacion <= 8) {
+      console.log("aprobado");
+    } else if (calificacion > 10 || calificacion < 0) {
+      console.log("Calificacion Invalida");
+    } else {
+      console.log("reprobado");
+    }
+
+}
