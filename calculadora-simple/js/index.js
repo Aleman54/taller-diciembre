ee572188d6bfee4693404a464d54fceaea17ae7e
JorgeLuis <estebanracing21@gmail.com>
2022-02-10 20:44:23 -0600

    subiendo calculadora simple

---
+++ calculadora-simple/js/index.js
@@ -0,0 +1,9 @@
+function suma() {
+  let operandoa = document.getElementById("primer-numero").value
+  console.log(operandoa)
+  let operandob = document.getElementById("segundo-numero").value
+  console.log(operandob)
+  let resultado = Number(operandoa)+ Number(operandob)
+  console.log("el resultado es", resultado);
+ 
+}
