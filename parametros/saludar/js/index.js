27a7fdf6d24064f429c5bca09fc4364e261dae4f
JorgeLuis <estebanracing21@gmail.com>
2022-02-10 21:32:04 -0600

    parametros

--- parametros/saludar/js/index.js
+++ parametros/saludar/js/index.js
@@ -1,10 +1,17 @@
 
-function saludar(n) {
+function saludar(n,edad,pais) {
   let caja = document.getElementById('caja-saludo')
-  caja.innerHTML = 'Hola, ' + n
+  caja.innerHTML = 'Hola, ' + n + ' tienes ' + edad + ' años de edad ' + ' y eres de ' + pais
   caja.style.textAlign = 'center'
+  if (pais === "mexico") {
+    caja.style.backgroundColor = 'green'
+    
+  } 
+  else {
+    caja.style.backgroundColor = 'purple'
+  }
+  
 }
-
 // saludar('Daniel');
 // saludar('Carlos');
 // saludar('Sergio')
