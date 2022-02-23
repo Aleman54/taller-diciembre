c0ba9f1b4b15dd9f9e2068fbdf8de01055285ac7
JorgeLuis <estebanracing21@gmail.com>
2022-02-22 19:12:43 -0600

    tabla de multiplicar actualizada

--- parametros/solo-js/multiplicar.js
+++ parametros/solo-js/multiplicar.js
@@ -1,10 +1,21 @@
 function obtenerTabla (tablax){
 console.log(tablax + ' x 1 =' + tablax * 1)
 console.log(tablax + " x 2 =" + tablax * 2)
-
+console.log(tablax + " x 3 =" + tablax * 3)
+console.log(tablax + " x 4 =" + tablax * 4)
+console.log(tablax + " x 5 =" + tablax * 5)
+console.log(tablax + " x 6 =" + tablax * 6)
+console.log(tablax + " x 7 =" + tablax * 7)
+console.log(tablax + " x 8 =" + tablax * 8)
+console.log(tablax + " x 9 =" + tablax * 9);
+console.log(tablax + " x 10 =" + tablax * 10)
 }
 obtenerTabla(5)
 // 5 x 1 = 5
 obtenerTabla(6)
 // 6 x 1 = 6
 obtenerTabla(1000)
+
+obtenerTabla(546)
+obtenerTabla(8950)
+obtenerTabla(75)
