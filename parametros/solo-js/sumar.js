c5b8003b9de10175b75e01231cd4c9ff740a05c6
JorgeLuis <estebanracing21@gmail.com>
2022-03-10 22:51:37 -0600

    actualizacion de suma

---
+++ parametros/solo-js/sumar.js
@@ -0,0 +1,17 @@
+function sumarHasta(limite){
+  let acumulador = 0
+  for (let i = 1; i <= limite; i = i + 1 ) {
+   // console.log("sumando ", acumulador , "+" , i , "...")
+    acumulador = acumulador + i
+
+  }
+  console.log("la suma total es ", acumulador)
+  console.log("ciclo terminado")
+
+}
+  
+  //hacer funcion obtener cuadrado hasta 4
+
+
+sumarHasta(5)
+sumarHasta(4)
